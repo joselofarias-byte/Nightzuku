23c5fcefdd2222971df69bff1046c49a47e3e38b
joselofarias-byte <289238002+joselofarias-byte@users.noreply.github.com>
2026-09-12 15:26:18 -0300

    Fix runtime package identity references

--- manager/src/main/jni/starter.cpp
+++ manager/src/main/jni/starter.cpp
@@ -31,7 +31,7 @@
 #define EXIT_FATAL_KILL 9
 #define EXIT_FATAL_BINDER_BLOCKED_BY_SELINUX 10
 
-#define PACKAGE_NAME "kerneldroid.nightzuku"
+#define PACKAGE_NAME "com.joselofarias.nightzuku"
 #define SERVER_NAME "shizuku_server"
 #define SERVER_CLASS_PATH "rikka.shizuku.server.ShizukuService"
 
